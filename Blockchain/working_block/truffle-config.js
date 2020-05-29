d077fc4ae4b5ee1178a6e9866338453590774bde
Samarth175 <samarth1751997@gmail.com>
2020-05-30 03:21:41 +0530

    added configuration for azure deployment

--- Blockchain/working_block/truffle-config.js
+++ Blockchain/working_block/truffle-config.js
@@ -1,5 +1,8 @@
 require('babel-register');
 require('babel-polyfill');
+const Web3 = require('web3');
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const fs = require("fs");
 
 module.exports = {
   networks: {
@@ -8,9 +11,20 @@ module.exports = {
       port: 7545,
       network_id: "*" // Match any network id
     },
+    westlake: {
+			// provider: new Web3.providers.HttpProvider("https://mymember2.blockchain.azure.com:3200/kz9_V3YJq14HosDuQiQBY6Qz"),
+      // host: "https://mymember2.blockchain.azure.com",
+      // port: 3200,
+      provider: new HDWalletProvider(fs.readFileSync('/home/codersam/itsmine/nodejs_projects/Blockchain-for-Medical-Records/Blockchain/working_block/myblockchainmember.env', 'utf-8'), "https://mymember2.blockchain.azure.com:3200/kz9_V3YJq14HosDuQiQBY6Qz"),
+      network_id: "*",
+			gas: 0,
+			gasPrice: 0
+      // from: "mymember2"
+      // networkCheckTimeout: 10000
+		},
   },
-  contracts_directory: '/home/leo/working_block/src/contracts/',
-  contracts_build_directory: '/home/leo/working_block/src/abis/',
+  contracts_directory: '/home/codersam/itsmine/nodejs_projects/Blockchain-for-Medical-Records/Blockchain/working_block/src/contracts/',
+  contracts_build_directory: '/home/codersam/itsmine/nodejs_projects/Blockchain-for-Medical-Records/Blockchain/working_block/src/abis/',
   compilers: {
     solc: {
       optimizer: {
